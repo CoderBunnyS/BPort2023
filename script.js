1668bd4bad7136b503208a11d7903c0f06e9127d
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-05-11 13:59:15 -0700

    styling changes

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   alert('Welcome to My Portfolio!');
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.querySelector('.navbar');
+    var sticky = navbar ? navbar.offsetTop : 0; // Get the offset top of the navbar or default to 0 if navbar is not found
+
+    window.onscroll = function() {
+        // Add "sticky" to navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+        if (navbar && window.scrollY >= sticky) {
+            navbar.classList.add("sticky");
+        } else if (navbar) {
+            navbar.classList.remove("sticky");
+        }
+    };
+});
+
+
